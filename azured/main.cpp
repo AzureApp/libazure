88c6f6ee33e39a7052084214d56e998ab5bca81c
Callum Taylor <callum@Callums-Mac.local>
2013-08-19 19:46:00 +0100

    Initial Commit

---
+++ azured/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  azured
+//
+//  Created by Callum Taylor on 19/08/2013.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+	return 0;
+}
+
