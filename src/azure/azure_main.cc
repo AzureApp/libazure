d29c3f7ae5c75deb8a030b2e90f81e9754ecbaeb
Satori <comfycallum@gmail.com>
2017-10-19 18:58:43 +0100

    Create azure command-line tool

---
+++ src/azure/azure_main.cc
@@ -0,0 +1,57 @@
+/**
+ ******************************************************************************
+ * Azure : Open Source iOS Memory Editor                                      *
+ * File : daemon_main.cc                                                      *
+ ******************************************************************************
+ * Copyright 2017 Satori. All rights reserved.                                *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#include <iostream>
+#include <vector>
+
+#include <gflags/gflags.h>
+#include <logging.h>
+
+#include <data_objects/data_object.h>
+#include <data_objects/search_object.h>
+#include <daemon.h>
+
+
+DEFINE_bool(daemon, false, "Run azure as a daemon");
+
+namespace azure {
+
+extern "C" int msgpack_main(int argc, char **argv) {
+    msgpack::sbuffer buffer;
+
+    SearchObject so(0x12345678);
+    msgpack::pack(buffer, so);
+
+    msgpack::object_handle oh = msgpack::unpack(buffer.data(), buffer.size());
+
+    msgpack::object obj = oh.get();
+    DataObject dataObj = obj.convert();
+
+    std::cout << "type: " << dataObj.type << std::endl;
+
+    SearchObject so2 = obj.convert();
+    std::cout << "type: " << so2.type << " address: 0x" << std::hex << so2.addr << " " << std::endl;
+}
+
+extern "C" int main(int argc, char **argv) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    if (FLAGS_daemon) {
+        AZLog("Starting azure in daemon mode");
+        Daemon daemon(argc, argv);
+        return daemon.Run();
+    } else {
+        AZLog("Starting azure in command-line mode");
+    }
+
+    msgpack_main(argc, argv);
+}
+
+}
---
+++ src/libazure/daemon.h
@@ -0,0 +1,35 @@
+/**
+ ******************************************************************************
+ * Azure : Open Source iOS Memory Editor                                      *
+ * File : daemon.h                                                            *
+ ******************************************************************************
+ * Copyright 2017 Satori. All rights reserved.                                *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#ifndef AZURE_DAEMON_H
+#define AZURE_DAEMON_H
+
+#include <string>
+#include "tcp_server.h"
+
+namespace azure {
+
+class Daemon {
+public:
+    Daemon(int argc, char **argv);
+    Daemon(int argc, char **argv, std::string ip, short port);
+    ~Daemon();
+
+    int Run();
+private:
+    int argc_;
+    char **argv_;
+
+    TCPServer server_;
+};
+
+}
+
+#endif //AZURE_DAEMON_H
