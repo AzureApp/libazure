c0b9a34af2432c815bf82be1f423c31ff53982f8
Razzile <comfycallum@gmail.com>
2017-10-27 11:53:52 +0100

    Add easy-use wrappers for TCP connection

--- src/libazure/tcp_conn.cc
+++ src/libazure/tcp_conn.cc
@@ -24,12 +24,12 @@ TCPConn::~TCPConn() {
     close(sock_);
 }
 
-bool TCPConn::IsConnected() {
+bool TCPConn::IsConnected() const {
     fd_set rfd;
     FD_ZERO(&rfd);
     FD_SET(sock_, &rfd);
-    timeval tv = { 0 };
-    select(sock_+1, &rfd, 0, 0, &tv);
+    timeval tv = {0};
+    select(sock_ + 1, &rfd, 0, 0, &tv);
     if (!FD_ISSET(sock_, &rfd))
         return false;
     int n = 0;
@@ -37,16 +37,157 @@ bool TCPConn::IsConnected() {
     return n == 0;
 }
 
-bool TCPConn::RunLoop() {
-    char data[6];
-    strcpy(data, "ping\n");
-    if (send(sock_, data, sizeof(data), 0) != 6) {
-        AZLogE("Failed to send data. Closing socket");
+int TCPConn::DataAvailable() const {
+    fd_set rfd;
+    FD_ZERO(&rfd);
+    FD_SET(sock_, &rfd);
+    timeval tv = {0};
+    select(sock_ + 1, &rfd, 0, 0, &tv);
+    if (!FD_ISSET(sock_, &rfd))
         return false;
-    }
-    char buf[256];
-    recv(sock_, buf, 256, 0);
-    AZLog("Received data: %s", buf);
+    int n = 0;
+    ioctl(sock_, FIONREAD, &n);
+    return n;
+}
+
+int8_t TCPConn::ReadChar() const {
+    int8_t c;
+    ReadBuf(&c, sizeof(c));
+    return c;
+}
+
+uint8_t TCPConn::ReadUChar() const {
+    uint8_t c;
+    ReadBuf(&c, sizeof(c));
+    return c;
+}
+
+int16_t TCPConn::ReadShort() const {
+    short s;
+    ReadBuf(&s, sizeof(s));
+    return s;
+}
+
+uint16_t TCPConn::ReadUShort() const {
+    int16_t s;
+    ReadBuf(&s, sizeof(s));
+    return s;
+}
+
+int32_t TCPConn::ReadInt() const {
+    int32_t i;
+    ReadBuf(&i, sizeof(i));
+    return i;
+}
+
+uint32_t TCPConn::ReadUInt() const {
+    uint32_t i;
+    ReadBuf(&i, sizeof(i));
+    return i;
+}
+
+int64_t TCPConn::ReadLong() const {
+    int64_t l;
+    ReadBuf(&l, sizeof(l));
+    return l;
+}
+
+uint64_t TCPConn::ReadULong() const {
+    uint64_t l;
+    ReadBuf(&l, sizeof(l));
+    return l;
+}
+
+size_t TCPConn::ReadBuf(void *buf, size_t size, size_t *out) const {
+    return recv(sock_, buf, size, 0);
+}
+
+int8_t TCPConn::PeekChar() const {
+    int8_t c;
+    PeekBuf(&c, sizeof(c));
+    return c;
+}
+
+uint8_t TCPConn::PeekUChar() const {
+    uint8_t c;
+    PeekBuf(&c, sizeof(c));
+    return c;
+}
+
+int16_t TCPConn::PeekShort() const {
+    short s;
+    PeekBuf(&s, sizeof(s));
+    return s;
+}
+
+uint16_t TCPConn::PeekUShort() const {
+    int16_t s;
+    PeekBuf(&s, sizeof(s));
+    return s;
+}
+
+int32_t TCPConn::PeekInt() const {
+    int32_t i;
+    PeekBuf(&i, sizeof(i));
+    return i;
+}
+
+uint32_t TCPConn::PeekUInt() const {
+    uint32_t i;
+    PeekBuf(&i, sizeof(i));
+    return i;
+}
+
+int64_t TCPConn::PeekLong() const {
+    int64_t l;
+    PeekBuf(&l, sizeof(l));
+    return l;
+}
+
+uint64_t TCPConn::PeekULong() const {
+    uint64_t l;
+    PeekBuf(&l, sizeof(l));
+    return l;
+}
+
+size_t TCPConn::PeekBuf(void *buf, size_t size) const {
+    recv(sock_, buf, size, MSG_PEEK);
+}
+
+void TCPConn::WriteChar(int8_t c) {
+    WriteBuf(&c, sizeof(c));
+}
+
+void TCPConn::WriteUChar(uint8_t c) {
+    WriteBuf(&c, sizeof(c));
+}
+
+void TCPConn::WriteShort(int16_t s) {
+    WriteBuf(&s, sizeof(s));
+}
+
+void TCPConn::WriteUShort(uint16_t s) {
+    WriteBuf(&s, sizeof(s));
+}
+
+void TCPConn::WriteInt(int32_t i) {
+    WriteBuf(&i, sizeof(i));
+}
+
+void TCPConn::WriteUInt(uint32_t i) {
+    WriteBuf(&i, sizeof(i));
+}
+
+void TCPConn::WriteLong(int64_t l) {
+    WriteBuf(&l, sizeof(l));
+}
+
+void TCPConn::WriteULong(uint64_t l) {
+    WriteBuf(&l, sizeof(l));
+}
+
+size_t TCPConn::WriteBuf(void *buf, size_t size) {
+    send(sock_, buf, size, 0);
 }
 
 }
--- src/libazure/tcp_conn.h
+++ src/libazure/tcp_conn.h
@@ -11,6 +11,8 @@
 #ifndef AZURE_TCP_CONN_H
 #define AZURE_TCP_CONN_H
 
+#include <stdint.h>
+
 namespace azure {
 
 class TCPConn {
@@ -18,14 +20,63 @@ public:
     TCPConn(int sock);
     ~TCPConn();
 
-    bool IsConnected();
+    bool IsConnected() const;
+    int DataAvailable() const;
+    int8_t ReadChar() const;
+    uint8_t ReadUChar() const;
+    int16_t ReadShort() const;
+    uint16_t ReadUShort() const;
+    int32_t ReadInt() const;
+    uint32_t ReadUInt() const;
+    int64_t ReadLong() const;
+    uint64_t ReadULong() const;
+    size_t ReadBuf(void *buf, size_t size) const;
 
-    bool RunLoop();
+    int8_t PeekChar() const;
+    uint8_t PeekUChar() const;
+    int16_t PeekShort() const;
+    uint16_t PeekUShort() const;
+    int32_t PeekInt() const;
+    uint32_t PeekUInt() const;
+    int64_t PeekLong() const;
+    uint64_t PeekULong() const;
+    size_t PeekBuf(void *buf, size_t size) const;
+
+    void WriteChar(int8_t c);
+    void WriteUChar(uint8_t c);
+    void WriteShort(int16_t s);
+    void WriteUShort(uint16_t s);
+    void WriteInt(int32_t i);
+    void WriteUInt(uint32_t i);
+    void WriteLong(int64_t l);
+    void WriteULong(uint64_t l);
+    size_t WriteBuf(void *buf, size_t size);
+
+
+    template <typename T> T Read();
+    template <typename T> T Peek();
+    template <typename T> void Write(T val);
 
 private:
     int sock_;
 };
 
+template <typename T> T TCPConn::Read() {
+    T temp;
+    ReadBuf(&temp, sizeof(T));
+    return temp;
+}
+
+template <typename T> T TCPConn::Peek() {
+    T temp;
+    PeekBuf(&temp, sizeof(T));
+    return temp;
+}
+
+template <typename T> void TCPConn::Write(T val) {
+    WriteBuf(&val, sizeof(T));
+}
+    
 }
 
 #endif //AZURE_TCP_CONN_H
