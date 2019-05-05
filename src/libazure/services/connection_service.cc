dac1ab157cd7371d57ce4312d24768ffe1e7a1d4
Satori <comfycallum@gmail.com>
2019-05-05 19:41:43 +0100

    Add basic logging to connect service rpc

--- src/libazure/services/connection_service.cc
+++ src/libazure/services/connection_service.cc
@@ -1,10 +1,18 @@
 #include "connection_service.h"
-
+#include <iostream>
 namespace azure {
 
 Status ConnectionServiceImpl::Connect(ServerContext* context,
                                       const proto::Handshake* request,
                                       proto::Handshake* response) {
+  const proto::DeviceInfo& info = request->device_info();
+  std::cout << "Azure version: " << request->azure_version() << std::endl;
+  std::cout << "Device Info"
+            << "\n\t Operating System (value): " << info.operating_system()
+            << "\n\t OS Version: " << info.os_version()
+            << "\n\t Device name: " << info.device_name()
+            << "\n\t UDID: " << info.udid();
+
   return Status(grpc::StatusCode::DO_NOT_USE, "Unimplemented");
 }
 
